a676a843894ed354e7b542652a3bdd8148e3c92a
Aman Jha <amank837197@gmail.com>
2026-07-10 10:47:18 +0530

    loops.js me chages

---
+++ control_flow/loop.js
@@ -0,0 +1,7 @@
+/*loop
+Loop defines a block of code that will be executed as long as a specified condition is true. The loop will continue to execute until the condition evaluates to false. There are several types of loops in JavaScript, including for loops, while loops, and do...while loops.
+
+1. For Loop: The for loop is used to iterate over a block of code a specific number of times. It consists of three parts: initialization, condition, and increment/decrement.
+ syntax: for (initialization; condition; increment/decrement) {
+    // code block to be executed
+} */
